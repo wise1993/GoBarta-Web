3b70737f2dc24c9f598da4b000c2da803831035d
Austin Ojukwu <austinwise1993@Austins-MacBook-Pro.local>
2021-08-11 17:55:30 +0100

    Bug fixed and site up to date

--- index.php
+++ index.php
@@ -1,138 +1,146 @@
 <?php
+$emailErr = '';
+$emailSuccess = '';
 
-    require 'vendor/autoload.php';
-    use \Mailjet\Resources;
-    $mj = new \Mailjet\Client('f9b62b31d965d7dc8dbaa6c5f2af0595','67fd5400838f14f345ab2bc148b3193c',true,['version' => 'v3.1']);
-    $emailErr = '';
-    $emailSuccess = '';
+if(isset($_POST['signup'])) {
 
-    if(isset($_POST['signup'])) {
-        $emailErr = '';
-        $email = $_POST["email"];
+$emailErr = '';
+$email = $_POST["email"];
+$name = $_POST['name']; 
 
-        $body = [
-        'Messages' => [
+$body = [
+    'Messages' => [
+        [
+        'From' => [
+            'Email' => "noreply@gobarta.com",
+            'Name' => "GoBarta Team"
+        ],
+        'To' => [
             [
-                'From' => [
-                    'Email' => "noreply@gobarta.com",
-                    'Name' => "GoBarta Team"
-                ],
-                'To' => [
-                    [
-                        'Email' => $email,
-                    ]
-                ],
-                'Subject' => "Thank you for subscribing to our early access.",
-                'TextPart' => "Signup for Early Access",
-                'HTMLPart' => "
-                   <section style='background:#E5E5E5'>
-                        <div style='max-width: 900px; margin: auto;'>
-                            <br />
-                            <div>
-                                <img style='width: 187px;' src='http://gobarta.com/assets/images/logo-mail.png'>
-                            </div>
-                            <br />
-
-                            <div style='background:white; padding:2em; border:1px solid #ffffff;'>
-                                <div style='text-align:center'>
-                                    <h2 style='align-items: center;text-align: center;letter-spacing: -1.5px;color: #212B4B;'>
-                                        We’re excited to have you here!</h2>
-                                    <h3>Welcome to GoBarta</h3>
-                                </div>
-                                <img style='width: 100%;' src='http://gobarta.com/assets/images/handshake.png'>
-                                <br /> <br />
-                                <span>Hello $email</span>
-
-                                <p
-                                    style='font-family: DM Sans;font-style: normal;font-weight: normal;font-size: 20px;line-height: 26px;display: flex; align-items: center;letter-spacing: -1px; color: #2C2738;'>
-                                    We may need to send you important information on your account and it is important that we have an
-                                    accurate email address.
-                                    <br /> <br />
-                                    You are required to verify your email to activate your account.<br /> <br />
-
-                                    Copy the code below and paste it in your mobile application or click on the link below <br> <br />
-
-                                    & 6 charactered code. <br> <br />
-
-                                    Thank you for choosing us, we are glad to be a part of your investment journey.
-                                </p>
-
-                                <button
-                                    style='color:#1C1F26; background: #00D57C;border-radius: 10px;padding: 10px; border: none;'>Confirm
-                                    Email Address</button>
-                                <br /> <br />
-
-                                <hr>
-                                <br />
-                                <div>
-                                    <img style='width: 187px' src='http://gobarta.com/assets/images/logo-mail.png'>
-                                </div>
-                            </div>
-                        </div>
-
-                        <br>
-
-                        <div style='max-width: 900px; margin: auto; text-align: center'>
-                            <div style='background:white; padding:1em; border:1px solid #ffffff;'>
-                                <ul style='display:inline-flex; padding-left: 0;'>
-                                    <li style='margin-right: 2em;display: block;'>HELP CENTER</li>
-                                    <li style='margin-right: 2em;display: block;'> SUPPORT 24/7 </li>
-                                    <li style='margin-right: 2em;display: block;'>ACCOUNT</li>
-                                </ul>
-
-                                <p>Copyright © 2017 GoBarta. All Rights Reserved. We appreciate you!</p>
-
-                                <ul style='display:inline-flex;padding-left: 0;'>
-                                    <li style='margin-right: 2em;display: block;'><span style='margin-right: 10px;'>help@gobarta.com
-                                        </span>|</li>
-                                    <li style='margin-right: 2em;display: block;'><span
-                                            style='margin-right: 10px;'>1(800)232-90-26</span> |
-                                    </li>
-                                    <li style='margin-right: 2em;display: block;'><a href=''>Unsubscribe</a></li>
-                                </ul> <br>
-
-                                <ul style='display:inline-flex; text-align:center;padding-left: 0;'>
-                                    <li style='margin-right: 2em;display: block;'><a href='https://www.instagram.com/gobarta_'
-                                            target='_blank'>
-                                            <img style='width:20px' src='http://gobarta.com/assets/images/icon-instagram.png'>
-                                        </a></li>
-
-                                    <li style='margin-right: 2em;display: block;'><a href='https://twitter.com/gobarta1'
-                                            target='_blank'>
-                                            <img style='width:20px' src='http://gobarta.com/assets/images/icon-twitter.png'>
-                                        </a></li>
-                                    <li style='margin-right: 2em;display: block;'><a href='https://www.facebook.com/go.barta'
-                                            target='_blank'>
-                                            <img style='width:20px' src='http://gobarta.com/assets/images/icon-fb.png'>
-                                        </a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                    ",
-                'CustomID' => "AppGettingStartedTest"
+                'Email' => $email,
+                'Name' => $name,
             ]
-        ]]; 
-        
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          $emailErr =  "<span class='error'> Kindly enter a valid email address. </span>";
-           header("refresh: 3"); 
-           
-        } else {
-            $user_mail = $email;
-            $myfile = fopen("emaildoc.txt", "a") or die("Unable to open file!");
-            $txt = $user_mail."\n";
-            fwrite($myfile, $txt);
-            fclose($myfile);
-            $emailSuccess = "<span class='successMail')><img src='./assets/images/success-icon.png\'> Yay! You’re now on our early access list. </span>";
-            $response = $mj->post(Resources::$Email, ['body' => $body]);
-            $response->success() && var_dump($response->getData());
+        ],
+        'Subject' => "Thank you for subscribing to our early access.",
+        'TextPart' => "Signup for Early Access",
+        'HTMLPart' => "
+            <section style='background:#E5E5E5'>
+                <div style='max-width: 900px; margin: auto;'>
+                    <br />
+                    <div>
+                        <img style='width: 187px;' src='http://gobarta.com/assets/images/logo-mail.png'>
+                    </div>
+                    <br />
 
-            header("refresh: 3"); 
-        }
-         
-    }
+                    <div style='background:white; padding:2em; border:1px solid #ffffff;'>
+                        <div style='text-align:center'>
+                            <h2 style='align-items: center;text-align: center;letter-spacing: -1.5px;color: #212B4B;'>
+                                We’re excited to have you here!</h2>
+                            <h3>Welcome to GoBarta</h3>
+                        </div>
+                        <img style='width: 100%;' src='http://gobarta.com/assets/images/handshake.png'>
+                        <br /> <br />
+                        <span>Hello $email,</span>
+
+                        <p
+                            style='font-family: DM Sans;font-style: normal;font-weight: normal;font-size: 20px;line-height: 26px;display: flex; align-items: center;letter-spacing: -1px; color: #2C2738;'>
+                            We may need to send you important information on your account and it is important that we have an
+                            accurate email address.
+                            <br /> <br />
+                            You are required to verify your email to activate your account.<br /> <br />
+
+                            Copy the code below and paste it in your mobile application or click on the link below <br> <br />
+
+                            & 6 charactered code. <br> <br />
+
+                            Thank you for choosing us, we are glad to be a part of your investment journey.
+                        </p>
+
+                        <button style='color:#1C1F26; background: #00D57C;border-radius: 10px;padding: 10px; border: none;'>
+                            <a href='http://gobarta.com/'>Continue to Website</a>
+                        </button>
+
+                        <br /> <br />
+
+                        <hr>
+                        <br />
+                        <div>
+                            <img style='width: 187px' src='http://gobarta.com/assets/images/logo-mail.png'>
+                        </div>
+                    </div>
+                </div>
+
+                <br>
+
+                <div style='max-width: 900px; margin: auto; text-align: center'>
+                    <div style='background:white; padding:1em; border:1px solid #ffffff;'>
+                        <ul style='display:inline-flex; padding-left: 0;'>
+                            <li style='margin-right: 2em;display: block;'>HELP CENTER</li>
+                            <li style='margin-right: 2em;display: block;'> SUPPORT 24/7 </li>
+                            <li style='margin-right: 2em;display: block;'>ACCOUNT</li>
+                        </ul>
+
+                        <p>Copyright © 2017 GoBarta. All Rights Reserved. We appreciate you!</p>
+
+                        <ul style='display:inline-flex;padding-left: 0;'>
+                            <li style='margin-right: 2em;display: block;'><span style='margin-right: 10px;'>help@gobarta.com
+                                </span>|</li>
+                            <li style='margin-right: 2em;display: block;'><span
+                                    style='margin-right: 10px;'>1(800)232-90-26</span> |
+                            </li>
+                            <li style='margin-right: 2em;display: block;'><a href=''>Unsubscribe</a></li>
+                        </ul> <br>
+
+                        <ul style='display:inline-flex; text-align:center;padding-left: 0;'>
+                            <li style='margin-right: 2em;display: block;'><a href='https://www.instagram.com/gobarta_'
+                                    target='_blank'>
+                                    <img style='width:20px' src='http://gobarta.com/assets/images/icon-instagram.png'>
+                                </a></li>
+
+                            <li style='margin-right: 2em;display: block;'><a href='https://twitter.com/gobarta1'
+                                    target='_blank'>
+                                    <img style='width:20px' src='http://gobarta.com/assets/images/icon-twitter.png'>
+                                </a></li>
+                            <li style='margin-right: 2em;display: block;'><a href='https://www.facebook.com/go.barta'
+                                    target='_blank'>
+                                    <img style='width:20px' src='http://gobarta.com/assets/images/icon-fb.png'>
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            ",
+        ]
+    ]
+];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.mailjet.com/v3.1/send");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_USERPWD, "f9b62b31d965d7dc8dbaa6c5f2af0595:67fd5400838f14f345ab2bc148b3193c");
+$server_output = curl_exec($ch);
+curl_close ($ch);
+  
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailErr =  "<span class='error'> Kindly enter a valid email address. </span>";    
+} else {
+    $user_mail = $email;
+    $myfile = fopen("emaildoc.txt", "a") or die("Unable to open file!");
+    $txt = $user_mail."\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    $emailSuccess = "<span class='successMail'><img src='http://gobarta.com/assets/images/success-icon.png'> Yay! You’re now on our early access list. </span>";
+    $response = json_decode($server_output);
+    header("refresh: 5"); 
+}
+
+}
 ?>
+
 
 <html>
 
@@ -171,7 +179,6 @@
 
                         <!-- logo for small screen -->
                         <img class="d-md-none" src="assets/images/logo.svg"  />
-                        <!-- <img class="d-md-none" src="assets/images/logoSm.svg"  /> -->
                         </a>
                     </span>
                 </div>
@@ -454,12 +461,19 @@
     <script src="assets/theme/js/script.js"></script>
     <!-- Scripts end -->
 
-    <!-- Cookies -->
-    <input name="cookieData" type="hidden" data-cookie-customDialogSelector='null' data-cookie-colorText='#424a4d'
+   <!-- Cookies -->
+    <input onclick="myFunction()" name="cookieData" type="hidden" data-cookie-customDialogSelector='null' data-cookie-colorText='#424a4d'
         data-cookie-colorBg='rgba(234, 239, 241, 0.99)' data-cookie-textButton='Agree' data-cookie-colorButton=''
         data-cookie-colorLink='#424a4d' data-cookie-underlineLink='true'
-        data-cookie-text="We use cookies to give you the best experience. Read our <a href='privacy.php'>cookie policy</a>.">
+        data-cookie-text="We use cookies to give you the best experience. Read our <a href='http://gobarta.com/privacy.php'>cookie policy</a>.">
     <!-- Cookies end -->
+
+    <script>
+        function myFunction() {
+            var x = "Cookies enabled: " + navigator.cookieEnabled;
+            document.getElementById("cookie").innerHTML = x;
+        }
+    </script>
 </body>
 
 </html>
